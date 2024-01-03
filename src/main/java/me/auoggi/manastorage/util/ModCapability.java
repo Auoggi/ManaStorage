09d87a8cdcb1d912741cc98b052473281eebeb5b
Auoggi <git@auoggi.com>
2024-01-03 22:05:58 +0100

    Rename ModCapability to ModCapabilityProvider and add support for multiple capabilities

--- src/main/java/me/auoggi/manastorage/util/ModCapability.java
+++ src/main/java/me/auoggi/manastorage/util/ModCapability.java
@@ -1,33 +1,15 @@
 package me.auoggi.manastorage.util;
 
-import net.minecraft.core.Direction;
 import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.ICapabilityProvider;
-import net.minecraftforge.common.util.LazyOptional;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.Objects;
 import java.util.function.Supplier;
 
-public class ModCapability implements ICapabilityProvider {
-    private final ICapabilityProvider parent;
-    private final Capability<?> capability;
-    private final Supplier<?> value;
+public class ModCapability {
+    public final Capability<?> capability;
+    public final Supplier<?> value;
 
-    public ModCapability(ICapabilityProvider parent, Capability<?> capability, Supplier<?> value) {
-        this.parent = parent;
-        this.capability = capability;
+    public ModCapability(Capability<?> capabilities, Supplier<?> value) {
+        this.capability = capabilities;
         this.value = value;
-    }
-
-    @NotNull
-    @Override
-    public <T> LazyOptional<T> getCapability(@NotNull Capability<T> capability, @Nullable Direction direction) {
-        if (this.capability == capability) {
-            return LazyOptional.of(() -> Objects.requireNonNull(this.value.get())).cast();
-        } else {
-            return this.parent != null ? this.parent.getCapability(capability, direction) : LazyOptional.empty();
-        }
     }
 }
---
+++ src/main/java/me/auoggi/manastorage/util/ModCapabilityProvider.java
@@ -0,0 +1,31 @@
+package me.auoggi.manastorage.util;
+
+import net.minecraft.core.Direction;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.util.LazyOptional;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+public class ModCapabilityProvider implements ICapabilityProvider {
+    private final ICapabilityProvider parent;
+    private final List<ModCapability> capabilities;
+
+    public ModCapabilityProvider(ICapabilityProvider parent, List<ModCapability> capabilities) {
+        this.parent = parent;
+        this.capabilities = capabilities;
+    }
+
+    @NotNull
+    @Override
+    public <T> LazyOptional<T> getCapability(@NotNull Capability<T> capability, @Nullable Direction direction) {
+        for(ModCapability cap : capabilities) {
+            if(cap.capability == capability) return LazyOptional.of(() -> Objects.requireNonNull(cap.value.get())).cast();
+        }
+        return this.parent != null ? this.parent.getCapability(capability, direction) : LazyOptional.empty();
+    }
+}
