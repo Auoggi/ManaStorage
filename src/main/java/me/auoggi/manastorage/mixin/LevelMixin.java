ae2ebbeba9b4737fd044b6b4aa33708f4815642f
Auoggi <augustoggiahrens@gmail.com>
2024-01-07 12:26:33 +0100

    Finish ManaStorageTablet functionality, missing rendering of tooltip image

--- src/main/java/me/auoggi/manastorage/mixin/LevelMixin.java
+++ src/main/java/me/auoggi/manastorage/mixin/LevelMixin.java
@@ -1,10 +1,15 @@
 package me.auoggi.manastorage.mixin;
 
+import me.auoggi.manastorage.ManaStorage;
+import net.minecraft.core.GlobalPos;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.TickingBlockEntity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,13 +21,13 @@ import java.util.Iterator;
 //Priority of Integer.MAX_VALUE will ensure it is always dead last - see https://jenkins.liteloader.com/view/Other/job/Mixin/javadoc/org/spongepowered/asm/mixin/Mixin.html#priority--
 @Pseudo @Mixin(value = Level.class, remap = false, priority = Integer.MAX_VALUE)
 public class LevelMixin {
-    /*@Inject(method = "tickBlockEntities", at = @At(value = "HEAD"))
-    private void tickBlockEntities(CallbackInfo ci) {
-
-    }*/
+    @Shadow @Final private ResourceKey<Level> dimension;
 
     @Inject(method = "tickBlockEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/entity/TickingBlockEntity;tick()V"), locals = LocalCapture.CAPTURE_FAILHARD)
     private void tickBlockEntity(CallbackInfo ci, ProfilerFiller profilerfiller, Iterator<TickingBlockEntity> iterator, TickingBlockEntity tickingblockentity) {
+        GlobalPos pos = GlobalPos.of(dimension, tickingblockentity.getPos());
 
+        //Make sure there aren't any duplicate positions in ManaStorage.pendingLoadedBlockEntities, it's not really needed but there just in case
+        if(!ManaStorage.pendingLoadedBlockEntities.contains(pos)) ManaStorage.pendingLoadedBlockEntities.add(pos);
     }
 }
--- src/main/java/me/auoggi/manastorage/util/ManaStorageCoreClientData.java
+++ src/main/java/me/auoggi/manastorage/util/ManaStorageCoreClientData.java
@@ -1,3 +1,10 @@
 package me.auoggi.manastorage.util;
 
-public record ManaStorageCoreClientData(boolean powered, double manaFraction) {}
+import me.auoggi.manastorage.ManaStorage;
+import me.auoggi.manastorage.block.entity.BasicImporterBlockEntity;
+
+public record ManaStorageCoreClientData(boolean powered, double manaFraction) {
+    public static ManaStorageCoreClientData of(BasicImporterBlockEntity entity) {
+        return new ManaStorageCoreClientData(entity.getEnergyStorage().extractEnergy(ManaStorage.basicEnergyUsage, true) >= ManaStorage.basicEnergyUsage, entity.getManaStorage().getManaStoredFraction());
+    }
+}
