987d87fbf1570c8818509eb972db1da7b5b05649
Auoggi <git@auoggi.com>
2024-01-12 23:33:47 +0100

    Better method of getting ModBoundItem

--- src/main/java/me/auoggi/manastorage/util/ModBoundItem.java
+++ src/main/java/me/auoggi/manastorage/util/ModBoundItem.java
@@ -1,8 +1,17 @@
 package me.auoggi.manastorage.util;
 
 import net.minecraft.core.GlobalPos;
+import net.minecraft.world.item.ItemStack;
+import vazkii.botania.api.BotaniaForgeCapabilities;
 import vazkii.botania.api.item.ICoordBoundItem;
+
+import javax.annotation.Nullable;
 
 public interface ModBoundItem extends ICoordBoundItem {
     GlobalPos getBinding();
+
+    @Nullable
+    static ModBoundItem of(ItemStack stack) {
+        return stack.getCapability(BotaniaForgeCapabilities.COORD_BOUND_ITEM).orElse(null) instanceof ModBoundItem modBoundItem ? modBoundItem : null;
+    }
 }
