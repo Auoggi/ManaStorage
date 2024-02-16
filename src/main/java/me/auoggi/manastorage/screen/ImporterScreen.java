e5bdb3dd88106f2a63f5350f8b05049190b9dbf5
Auoggi <augustoggiahrens@gmail.com>
2024-02-16 17:26:18 +0100

    rename importer.png to porter.png because both importer and exporter will use it

--- src/main/java/me/auoggi/manastorage/screen/ImporterScreen.java
+++ src/main/java/me/auoggi/manastorage/screen/ImporterScreen.java
@@ -6,6 +6,6 @@ import net.minecraft.world.entity.player.Inventory;
 
 public class ImporterScreen extends BaseContainerScreen<ImporterMenu> {
     public ImporterScreen(ImporterMenu menu, Inventory inventory, Component component) {
-        super(menu, inventory, component, "textures/gui/importer.png");
+        super(menu, inventory, component, "textures/gui/porter.png");
     }
 }
